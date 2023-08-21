4c0c03bb8d1b754aae335e2d2985d7afbfebcc8c
Deshan Nawanjana <deshan.uok@gmail.com>
2023-08-22 01:08:47 +0530

    Initial Commit

---
+++ assets/modules/Notifications.js
@@ -0,0 +1,114 @@
+// local store key space
+const name = 'local-web-notifications'
+
+// notification data
+const data = { states: null, items: [] }
+
+// method to set on local storage
+const readLocal = () => {
+    // get data from storage
+    const text = localStorage.getItem(name) || '{}'
+    // return parsed object
+    return JSON.parse(text)
+}
+
+// method to set on local storage
+const saveLocal = () => {
+    // convert object to string
+    const text = JSON.stringify(data.states)
+    // store on storage
+    localStorage.setItem(name, text)
+}
+
+// method to resolve notification data
+const resolveOutput = (resolve, id = null) => {
+    // map notification items
+    data.items = data.items.map(item => {
+        // set is read flag
+        item.isRead = data.states[item.id.toString()]
+        // return same item
+        return item
+    })
+    // get unread count
+    const count = Object.values(data.states).filter(item => !item).length
+    // check current id
+    if (id !== null) {
+        // get notification by id
+        const current = data.items.find(item => {
+            // match with id
+            return item.id.toString() === id.toString()
+        })
+        // resolve output
+        resolve({ items: data.items, current, count })
+    } else {
+        // resolve output
+        resolve({ items: data.items, count })
+    }
+}
+
+// notifications module
+const Notifications = {}
+
+// method to register notifications
+Notifications.register = (items = []) => {
+    // return promise
+    return new Promise(resolve => {
+        // get previous states from local
+        data.states = readLocal()
+        // for each notification item
+        for (let i = 0; i < items.length; i++) {
+            // current item
+            const item = items[i]
+            // check none registered ids
+            if (item.id in data.states === false) {
+                // register new unread notification
+                data.states[item.id] = false
+            }
+        }
+        // get state registry ids
+        const ids = Object.keys(data.states)
+        // for each id
+        for (let i = 0; i < ids.length; i++) {
+            // current id
+            const id = ids[i].toString()
+            // check on notification items
+            if (items.some(item => item.id.toString() === id) === false) {
+                // delete unavailable states
+                delete data.states[id]
+            }
+        }
+        // set items on data
+        data.items = items
+        // save into local
+        saveLocal()
+        // resolve output
+        resolveOutput(resolve)
+    })
+}
+
+Notifications.markAsRead = id => {
+    // return promise
+    return new Promise(resolve => {
+        // update notification state
+        data.states[id.toString()] = true
+        // save into local
+        saveLocal()
+        // resolve output
+        resolveOutput(resolve, id)
+    })
+}
+
+Notifications.markAsUnread = id => {
+    // return promise
+    return new Promise(resolve => {
+        // update notification state
+        data.states[id.toString()] = false
+        // save into local
+        saveLocal()
+        // resolve output
+        resolveOutput(resolve, id)
+    })
+}
+
+// export module
+export default Notifications
---
+++ index.js
@@ -0,0 +1,96 @@
+import Notifications from "./assets/modules/Notifications"
+
+// ============================ mockups handling ============================
+
+// method to imitate api request
+const fetchNotifications = async () => {
+    // check edited notifications
+    if ('notifications-api' in localStorage) {
+        // return notifications from local
+        return JSON.parse(localStorage.getItem('notifications-api'))
+    } else {
+        // request notifications
+        const resp = await fetch('index.json')
+        // return parsed data
+        return await resp.json()
+    }
+}
+
+// ============================ notifications component ============================
+
+new Vue({
+    // element selector
+    el: '#app',
+    // app data
+    data: {
+        // open states
+        opened: {
+            // popup state
+            popup: true,
+            // current item state
+            item: null
+        },
+        // notification items
+        notifications: [],
+        // unread count
+        count: 0,
+        // busy state
+        busy: false,
+        // realtime mode
+        realtime: true
+    },
+    // methods
+    methods: {
+        // method to toggle popup
+        togglePopup() {
+            // toggle popup opened state
+            this.opened.popup = !this.opened.popup
+        },
+        // method to load notifications
+        async loadNotifications() {
+            // return if busy or set busy state
+            if (this.busy) { return } { this.busy = true }
+            // request notification items
+            const items = await fetchNotifications()
+            // register notifications locally
+            Notifications.register(items).then(data => {
+                // set notification items
+                this.notifications = data.items
+                // set unread count
+                this.count = data.count
+                // clear busy state
+                this.busy = false
+            })
+        },
+        // method to open notification
+        openNotification(id) {
+            // set notification as read
+            Notifications.markAsRead(id).then(data => {
+                // update notification items
+                this.notifications = data.items
+                // set unread count
+                this.count = data.count
+                // set current item
+                this.opened.item = data.current
+            })
+        },
+        // method to close notification
+        closeNotification() {
+            // reset opened item id
+            this.opened.item = null
+        }
+    },
+    // mounted event
+    async mounted() {
+        // load notifications
+        this.loadNotifications()
+        // realtime loop
+        setInterval(() => {
+            // check realtime mode and busy state
+            if (this.realtime && this.busy == false) {
+                // load notifications
+                this.loadNotifications()
+            }
+        }, 3000)
+    }
+})
